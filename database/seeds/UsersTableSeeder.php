8f40bc065b1061fa58342e0de03f41541ce3d4cb
tsuruga_futaba <fs19930510@gmail.com>
2023-12-16 13:17:10 +0900

    no message

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            ['over_name'=>'佐藤',
+            'under_name'=>'太郎',
+            'over_name_kana'=>'サトウ',
+            'under_name_kana'=>'タロウ',
+            'mail_address'=>'satoutarou@com.com',
+            'sex'=>'1',
+            'birth_day'=>'2003_10_12',
+            'role'=>'1',
+            'password'=>'satoutarou20031012',
+            'remember_token'=>'123456',]
+        ]);
 
     }
 }
